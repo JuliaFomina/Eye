1e139cec52749d1b4e4f78f6f080624e9e9468a5
Julia Fomina <julia.fomina@studiomobile.ru>
2011-09-20 17:06:29 +0400

    add project files

---
+++ WeatherWidget/src/android/weather_widget/WeatherWidget.java
@@ -0,0 +1,19 @@
+package android.weather_widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: julia
+ * Date: 9/20/11
+ * Time: 11:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeatherWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+        int[] appWidgetIds){
+    }
+}
