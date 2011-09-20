2b6b494e8c13889c93dfa0ea2081e8fa90af20e9
Julia Fomina <julia.fomina@studiomobile.ru>
2011-09-20 18:39:52 +0400

    widget reacting on user touch

--- WeatherWidget/src/android/weather_widget/WeatherWidget.java
+++ WeatherWidget/src/android/weather_widget/WeatherWidget.java
@@ -3,6 +3,12 @@ package android.weather_widget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+import com.example.R;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,9 +17,17 @@ import android.content.Context;
  * Time: 11:51 AM
  * To change this template use File | Settings | File Templates.
  */
-public class WeatherWidget extends AppWidgetProvider {
+public class WeatherWidget extends AppWidgetProvider{
+
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
         int[] appWidgetIds){
     }
+
+//    public void onClick(View view){
+//        ImageView imageView = (ImageView) view.findViewById(R.id.widget_imageview);
+//        imageView.setImageResource(R.drawable.fluffy_pressed);
+//    }
+
 }
