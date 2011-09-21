8ba99344002cd64a11a9bf711a8a8c038f31a3e9
Julia Fomina <julia.fomina@studiomobile.ru>
2011-09-21 15:25:46 +0400

    Widget that show time hh:mm:ss in simple TextView.

--- Eye/src/com/studiomobile/EyeWidgetProvider.java
+++ Eye/src/com/studiomobile/EyeWidgetProvider.java
@@ -2,13 +2,43 @@ package com.studiomobile;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.widget.RemoteViews;
 
-public class EyeWidgetProvider extends AppWidgetProvider{
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class EyeWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-        int[] appWidgetIds){
+                         int[] appWidgetIds) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1, 1000);
     }
+
+    private class MyTime extends TimerTask {
+        RemoteViews remoteViews;
+        AppWidgetManager appWidgetManager;
+        ComponentName thisWidget;
+        java.text.DateFormat format = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM,
+                Locale.getDefault());
+
+        public MyTime(Context context, AppWidgetManager appWidgetManager) {
+            this.appWidgetManager = appWidgetManager;
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
+            thisWidget = new ComponentName(context, EyeWidgetProvider.class);
+        }
+
+        @Override
+        public void run() {
+            remoteViews.setTextViewText(R.id.widget_textview, format.format(new Date()));
+            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+        }
+
+    }
+
+
 }
